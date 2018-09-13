6b3c95b3bd9731e007865542c7c9bfb61e3eea60
Unknown <alfian853@gmail.com>
2018-09-13 23:41:19 +0700

    Implement update "telp" and "password"

--- src/main/java/com/bliblifuture/Invenger/controller/LoginController.java
+++ src/main/java/com/bliblifuture/Invenger/controller/LoginController.java
@@ -1,21 +1,22 @@
 package com.bliblifuture.Invenger.controller;
 
 import com.bliblifuture.Invenger.InvengerApplication;
-import com.bliblifuture.Invenger.model.Position;
+import com.bliblifuture.Invenger.annotation.imp.PasswordValdator;
+import com.bliblifuture.Invenger.annotation.imp.PhoneValidator;
 import com.bliblifuture.Invenger.model.User;
 import com.bliblifuture.Invenger.repository.PositionRepository;
-import javafx.geometry.Pos;
-import org.hibernate.Hibernate;
+import com.bliblifuture.Invenger.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
@@ -23,6 +24,9 @@ public class LoginController {
 
     @Autowired
     PositionRepository positionRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/login")
     public String getLogin(Model model){
@@ -44,8 +48,60 @@ public class LoginController {
     public String getProfile(Model model){
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         user.setPosition(positionRepository.findUserPosition(user.getId()));
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        log("testpwd: "+encoder.matches("root",user.getPassword()));
+        log("testpwd2: "+encoder.matches("Root123",user.getPassword()));
         model.addAttribute("user",user);
         return "user_profile";
     }
+
+    private void log(String s){
+        InvengerApplication.log.info(s);
+    }
+
+
+    @PostMapping("/profile")
+    public String postProfile(HttpServletRequest request){
+        log(request.getParameter("new-telp"));
+        log(Boolean.toString(request.getParameter("new-telp").equals("")));
+        log(Boolean.toString(request.getParameter("new-telp") == null));
+        log(request.getParameter("new-telp"));
+
+        User user = null;
+        if(!"".equals(request.getParameter("new-telp")) ){
+            if(PhoneValidator.isValid(request.getParameter("new-telp"))){
+                user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                user.setTelp(request.getParameter("telp"));
+                userRepository.save(user);
+            }
+            else{
+                log("phone number isnot valid");
+            }
+        }
+
+        if(!"".equals(request.getParameter("old-pwd")) ){
+            String newPassword = request.getParameter("new-pwd1");
+            if(!newPassword.equals(request.getParameter("new-pwd2")) ){
+                return "password doesn't match";
+            }
+            if(!PasswordValdator.isValid(newPassword)){
+                return "weak password";
+            }
+
+            if(user == null){
+                user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            }
+            BCryptPasswordEncoder bcyrpt = new BCryptPasswordEncoder();
+            if(!bcyrpt.matches(request.getParameter("old-pwd"),user.getPassword() )){
+                return "wrong password";
+            }
+
+            user.setPassword(bcyrpt.encode(newPassword));
+            userRepository.save(user);
+        }
+
+        return "user_profile";
+    }
+
 
 }
