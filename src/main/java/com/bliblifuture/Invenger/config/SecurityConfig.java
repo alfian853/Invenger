37132a9254f0f0b8e0aafb25bc5ad5ee72cf4124
nuzhamusyafira <nuzhamusyafira@gmail.com>
2018-09-20 10:38:42 +0700

    LOGOUT
    added logout, gotta fix the css

--- src/main/java/com/bliblifuture/Invenger/config/SecurityConfig.java
+++ src/main/java/com/bliblifuture/Invenger/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -70,8 +71,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login")
                 .and()
                 .rememberMe().tokenRepository(this.persistentTokenRepository())
-                .and()
-                .logout().logoutUrl("/logout").logoutSuccessUrl("/login")
+                // .and()
+                .and().logout()    //logout configuration
+                .logoutUrl("/logout") 
+                .logoutSuccessUrl("/login")
+                // .logout().logoutUrl("/logout").logoutSuccessUrl("/login")
                 .and()
                 .csrf();
 
