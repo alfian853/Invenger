8ae37d96e72ef509073489870f0bf4f5bf4f540c
Unknown <alfian853@gmail.com>
2018-09-30 13:50:30 +0700

    Transform MyUtils to @Component

--- src/main/java/com/bliblifuture/Invenger/Utils/MyUtils.java
+++ src/main/java/com/bliblifuture/Invenger/Utils/MyUtils.java
@@ -1,23 +1,28 @@
 package com.bliblifuture.Invenger.Utils;
 
+import com.bliblifuture.Invenger.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
+import java.security.Principal;
+
+@Component
 public class MyUtils {
 
-    private static BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-    private static final Logger logger = LoggerFactory.getLogger(MyUtils.class);
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    private final Logger logger = LoggerFactory.getLogger(MyUtils.class);
 
-    public static String getBcryptHash(String s){return bCryptPasswordEncoder.encode(s);}
-    public static boolean matches(String plain, String encoded){
+    public String getBcryptHash(String s){return bCryptPasswordEncoder.encode(s);}
+
+    public boolean matches(String plain, String encoded){
         return bCryptPasswordEncoder.matches(plain,encoded);
     }
 
-    public static void log(String s){
+    public void log(String s){
         logger.info(s);
     }
-
-
 
 }
