c195dc75d5549caa3d5713fabebed0b156322972
nuzhamusyafira <nuzhamusyafira@gmail.com>
2018-10-08 17:15:27 +0700

    fixed inventory service

--- src/main/java/com/bliblifuture/Invenger/service/InventoryService.java
+++ src/main/java/com/bliblifuture/Invenger/service/InventoryService.java
@@ -1,6 +1,8 @@
 package com.bliblifuture.Invenger.service;
 
 import com.bliblifuture.Invenger.model.Inventory;
+import com.bliblifuture.Invenger.model.Category;
+import com.bliblifuture.Invenger.repository.category.CategoryRepository;
 import com.bliblifuture.Invenger.repository.InventoryRepository;
 import com.bliblifuture.Invenger.request.formRequest.InventoryCreateRequest;
 import com.bliblifuture.Invenger.response.jsonResponse.RequestResponse;
@@ -28,11 +30,18 @@ public class InventoryService {
     // not implemented yet
     public RequestResponse createInventory(InventoryCreateRequest request){
         RequestResponse response = new RequestResponse();
+        Category newCategory = new Category();
+        newCategory.setId(request.getCategory_id());
+        System.out.println(request);
         Inventory newInventory = new Inventory();
         newInventory.setName(request.getName());
         newInventory.setQuantity(request.getQuantity());
         newInventory.setPrice(request.getPrice());
         newInventory.setDescription(request.getDescription());
+        newInventory.setCategory(newCategory);
+//        inventoryRepository.save(newCategory);
+//        newInventory.setCategory(request.);
+//        newInventory.setImage(request.getPhoto_file());
         inventoryRepository.save(newInventory);
         response.setStatusToSuccess();
         return response;
