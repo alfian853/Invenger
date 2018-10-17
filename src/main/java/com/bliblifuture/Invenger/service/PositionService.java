1525825c1ad76da559084f26e0a6c038948554d0
nuzhamusyafira <nuzhamusyafira@gmail.com>
2018-10-17 17:13:52 +0700

    add services for user requirements

---
+++ src/main/java/com/bliblifuture/Invenger/service/PositionService.java
@@ -0,0 +1,19 @@
+package com.bliblifuture.Invenger.service;
+
+import com.bliblifuture.Invenger.model.Position;
+import com.bliblifuture.Invenger.repository.PositionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PositionService {
+
+    @Autowired
+    PositionRepository positionRepository;
+
+    public List<Position> getAllPosition(){
+        return positionRepository.findAll();
+    }
+}
