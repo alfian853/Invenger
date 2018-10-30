422b9b0a460313d8ad74ce4532e83ef870f28791
alfian <alfian853@gmail.com>
2018-10-30 23:08:21 +0700

    add new dependency and add model mapper for lendment

---
+++ src/main/java/com/bliblifuture/Invenger/ModelMapper/lendment/LendmentMapper.java
@@ -0,0 +1,24 @@
+package com.bliblifuture.Invenger.ModelMapper.lendment;
+
+import com.bliblifuture.Invenger.model.lendment.Lendment;
+import com.bliblifuture.Invenger.model.lendment.LendmentDetail;
+import com.bliblifuture.Invenger.response.viewDto.LendmentDTO;
+import com.bliblifuture.Invenger.response.viewDto.LendmentDetailDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface LendmentMapper {
+
+    LendmentDTO toLendmentDTO(Lendment lendment);
+
+    LendmentDetailDTO toLendmentDetailDTO(LendmentDetail lendment);
+
+    List<LendmentDTO> toLendmentDtoList(List<Lendment> lendments);
+
+    List<LendmentDetailDTO> toLendmentDetailDtoList(List<LendmentDetail> lendmentDetails);
+
+
+}
---
+++ src/main/java/com/bliblifuture/Invenger/ModelMapper/lendment/LendmentMapperImpl.java
@@ -0,0 +1,48 @@
+package com.bliblifuture.Invenger.ModelMapper.lendment;
+
+import com.bliblifuture.Invenger.model.lendment.Lendment;
+import com.bliblifuture.Invenger.model.lendment.LendmentDetail;
+import com.bliblifuture.Invenger.response.viewDto.LendmentDTO;
+import com.bliblifuture.Invenger.response.viewDto.LendmentDetailDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LendmentMapperImpl implements LendmentMapper {
+
+    @Override
+    public LendmentDTO toLendmentDTO(Lendment lendment) {
+        return LendmentDTO
+                .builder()
+                .id(lendment.getId())
+                .username(lendment.getUser().getUsername())
+                .status(lendment.getStatus())
+                .orderDate(lendment.getCreatedAt())
+                .build();
+    }
+
+    @Override
+    public LendmentDetailDTO toLendmentDetailDTO(LendmentDetail lendment) {
+        return LendmentDetailDTO
+                .builder()
+                .inventoryId(lendment.getInventory().getId())
+                .inventoryName(lendment.getInventory().getName())
+                .quantity(lendment.getQuantity())
+                .isReturned(lendment.isReturned())
+                .returnDate(lendment.getReturnDate() != null ? lendment.getReturnDate().toString() : null)
+                .build();
+    }
+
+
+    @Override
+    public List<LendmentDTO> toLendmentDtoList(List<Lendment> lendments) {
+        return lendments.stream().map(this::toLendmentDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<LendmentDetailDTO> toLendmentDetailDtoList(List<LendmentDetail> lendmentDetails) {
+        return lendmentDetails.stream().map(this::toLendmentDetailDTO).collect(Collectors.toList());
+    }
+
+
+}
