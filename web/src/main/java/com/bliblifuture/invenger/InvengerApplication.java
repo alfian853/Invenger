022ef7d5fbc482bdfd36507c3fca0b8027f26c2e
Unknown <alfian853@gmail.com>
2018-12-15 17:20:53 +0700

    refactoring app

--- web/src/main/java/com/bliblifuture/invenger/InvengerApplication.java
+++ web/src/main/java/com/bliblifuture/invenger/InvengerApplication.java
@@ -1,11 +1,11 @@
 package com.bliblifuture.invenger;
 
+import com.bliblifuture.invenger.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class InvengerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InvengerApplication.class, args);
--- web/src/main/java/com/bliblifuture/invenger/config/MvcConfig.java
+++ web/src/main/java/com/bliblifuture/invenger/config/MvcConfig.java
@@ -1,12 +1,17 @@
 package com.bliblifuture.invenger.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@EnableJpaAuditing
 public class MvcConfig implements WebMvcConfigurer {
 
     final String BASE_PATH = "file:web/src/main/resources";
