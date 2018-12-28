0c0a41d8a5bfefb308275ac5c21ac92024c58438
nuzhamusyafira <nuzhamusyafira@gmail.com>
2018-12-28 16:18:31 +0700

    fixed some conflict

--- web/src/test/java/com/bliblifuture/invenger/controller/InventoryControllerTest.java
+++ web/src/test/java/com/bliblifuture/invenger/controller/InventoryControllerTest.java
@@ -3,11 +3,6 @@ package com.bliblifuture.invenger.controller;
 import com.bliblifuture.invenger.entity.inventory.Category;
 import com.bliblifuture.invenger.entity.inventory.Inventory;
 import com.bliblifuture.invenger.entity.inventory.InventoryType;
-import com.bliblifuture.invenger.entity.user.User;
-import com.bliblifuture.invenger.exception.DataNotFoundException;
-import com.bliblifuture.invenger.exception.InvalidRequestException;
-import com.bliblifuture.invenger.request.datatables.DataTablesRequest;
-import com.bliblifuture.invenger.request.formRequest.InventoryCreateRequest;
 import com.bliblifuture.invenger.request.formRequest.InventoryEditRequest;
 import com.bliblifuture.invenger.response.jsonResponse.*;
 import com.bliblifuture.invenger.response.jsonResponse.search_response.SearchResponse;
@@ -21,29 +16,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.Principal;
 import java.util.*;
 
-import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.times;
@@ -113,36 +96,9 @@ public class InventoryControllerTest {
                 .andExpect(jsonPath("$.success").value(true));
     }
 
-<<<<<<< HEAD
     /////////////////////////////////////////////////////////////////////////////////////////////
     //public RequestResponse editInventory(@Valid @ModelAttribute InventoryEditRequest request)//
     /////////////////////////////////////////////////////////////////////////////////////////////
-=======
-    @Test
-    public void addNewInventory_invalidRequest() throws Exception {
-        Inventory inventory = Inventory.builder()
-                .id(1)
-                .name("name")
-                .type("Consumbale")
-                .quantity(4)
-                .build();
-
-        InventoryCreateResponse response = new InventoryCreateResponse();
-        response.setInventory_id(inventory.getId());
-        response.setStatusToSuccess();
-
-        when(inventoryService.createInventory(any())).thenThrow(new InvalidRequestException());
-
-        mvc.perform(post("/inventory/create")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("name","name")
-                .param("category_id", "1")
-                .param("type","Consumable")
-                .param("quantity","4")
-        )
-                .andExpect(status().isBadRequest());
-    }
->>>>>>> dea66e7b685d3949f9409f27e3b809cc0678f56d
 
     @Test
     public void editInventory_test() throws Exception {
@@ -188,70 +144,9 @@ public class InventoryControllerTest {
 
     }
 
-<<<<<<< HEAD
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //public SearchResponse searchInventory(@RequestParam("search")String query, @RequestParam("page")Integer page, @RequestParam("length")Integer length)//
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-=======
-    @Test
-    public void deleteInventory_invalidRequest() throws Exception {
-        when(inventoryService.deleteInventory(anyInt())).thenThrow(new InvalidRequestException());
-
-        mvc.perform(post("/inventory/delete/1"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.success").value(false));
-
-        verify(inventoryService,times(1)).deleteInventory(anyInt());
-
-    }
-
-    @Test
-    public void deleteInventory_dataNotFound() throws Exception {
-        when(inventoryService.deleteInventory(anyInt())).thenThrow(new DataNotFoundException());
-
-        mvc.perform(post("/inventory/delete/1"))
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.success").value(false));
-
-        verify(inventoryService,times(1)).deleteInventory(anyInt());
-
-    }
-
-    @Test
-    public void getInventoryDetail_success() throws Exception{
-        Category category = Category.builder().id(1).name("/all").build();
-        InventoryDTO inventory = InventoryDTO.builder()
-                .id(1)
-                .name("name")
-                .quantity(2)
-                .type(InventoryType.Consumable.toString())
-                .category(category.getName())
-                .category_id(category.getId())
-                .build();
-
-        System.out.println(inventory);
-
-        when(inventoryService.getById(1)).thenReturn(inventory);
-        when(userService.currentUserIsAdmin()).thenReturn(true);
-
-        mvc.perform(get("/inventory/detail/{id}", 1)
-        )
-                .andExpect(status().isOk());
-
-        verify(inventoryService, times(1)).getById(1);
-    }
-
-    @Test
-    public void getInventoryDetail_dataNotFound() throws Exception{
-        when(inventoryService.getById(1)).thenThrow(new DataNotFoundException());
-
-        mvc.perform(get("/inventory/detail/{id}", 1))
-                .andExpect(status().isNotFound());
-
-        verify(inventoryService, times(1)).getById(1);
-        Mockito.verifyZeroInteractions(inventoryService);
-    }
->>>>>>> dea66e7b685d3949f9409f27e3b809cc0678f56d
 
     @Test
     public void searchInventory_test() throws Exception {
