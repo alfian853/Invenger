8224d803315de74bd1f275f3d077911ee599dda4
nuzhamusyafira <nuzhamusyafira@gmail.com>
2018-12-28 15:58:08 +0700

    more coverage on inventory

--- web/src/test/java/com/bliblifuture/invenger/controller/InventoryControllerTest.java
+++ web/src/test/java/com/bliblifuture/invenger/controller/InventoryControllerTest.java
@@ -6,12 +6,10 @@ import com.bliblifuture.invenger.entity.inventory.InventoryType;
 import com.bliblifuture.invenger.entity.user.User;
 import com.bliblifuture.invenger.exception.DataNotFoundException;
 import com.bliblifuture.invenger.exception.InvalidRequestException;
+import com.bliblifuture.invenger.request.datatables.DataTablesRequest;
 import com.bliblifuture.invenger.request.formRequest.InventoryCreateRequest;
 import com.bliblifuture.invenger.request.formRequest.InventoryEditRequest;
-import com.bliblifuture.invenger.response.jsonResponse.InventoryCreateResponse;
-import com.bliblifuture.invenger.response.jsonResponse.InventoryDocDownloadResponse;
-import com.bliblifuture.invenger.response.jsonResponse.RequestResponse;
-import com.bliblifuture.invenger.response.jsonResponse.UploadProfilePictResponse;
+import com.bliblifuture.invenger.response.jsonResponse.*;
 import com.bliblifuture.invenger.response.jsonResponse.search_response.SearchResponse;
 import com.bliblifuture.invenger.response.viewDto.CategoryDTO;
 import com.bliblifuture.invenger.response.viewDto.InventoryDTO;
@@ -35,10 +33,13 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.*;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -58,6 +59,9 @@ public class InventoryControllerTest {
     private MockMvc mvc;
 
     @Mock
+    private ItemCategoryService itemCategoryService;
+
+    @Mock
     private UserService userService;
 
     @Mock
@@ -75,20 +79,12 @@ public class InventoryControllerTest {
 
     }
 
-    ////////////////////////////////////////////////
-    //public String getInventoryTable(Model model)//
-    ////////////////////////////////////////////////
-
-    @Test
-    public void getInventoryTable() throws Exception {
-    }
-
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
     //public InventoryCreateResponse addNewInventory(@Valid @ModelAttribute InventoryCreateRequest request)//
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Test
-    public void addNewInventory_success() throws Exception {
+    public void addNewInventory_test() throws Exception {
         Category category = new Category().builder().id(1).name("/all").build();
         Inventory inventory = new Inventory().builder()
                 .id(1)
@@ -112,43 +108,17 @@ public class InventoryControllerTest {
                 .param("category_id", "1")
                 .param("type","Consumable")
                 .param("quantity","4")
-                .sessionAttr("inventory", new Inventory())
         )
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true));
     }
 
-    @Test
-    public void addNewInventory_invalidRequest() throws Exception {
-        Category category = new Category().builder().id(1).name("/all").build();
-        Inventory inventory = new Inventory().builder()
-                .id(1)
-                .name("name")
-                .category(category)
-                .type("string")
-                .quantity(4)
-                .build();
-
-        System.out.println(inventory);
-
-        InventoryCreateResponse response = new InventoryCreateResponse();
-        response.setInventory_id(inventory.getId());
-        response.setStatusToSuccess();
-
-        when(inventoryService.createInventory(any())).thenThrow(new InvalidRequestException());
-
-        mvc.perform(post("/inventory/create")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("name","name")
-                .param("category_id", "1")
-                .param("type","string")
-                .param("quantity","4")
-                .sessionAttr("inventory", new Inventory())
-        )
-                .andExpect(status().isInternalServerError());
-    }
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    //public RequestResponse editInventory(@Valid @ModelAttribute InventoryEditRequest request)//
+    /////////////////////////////////////////////////////////////////////////////////////////////
 
     @Test
-    public void editInventory_success() throws Exception {
+    public void editInventory_test() throws Exception {
         InventoryEditRequest request = new InventoryEditRequest();
         request.setId(1);
         request.setName("name2");
@@ -172,33 +142,12 @@ public class InventoryControllerTest {
 
     }
 
-    @Test
-    public void editInventory_invalidRequest() throws Exception {
-        InventoryEditRequest request = new InventoryEditRequest();
-        request.setId(1);
-        request.setName("name2");
-        request.setCategory_id(1);
-        request.setType(InventoryType.Consumable);
-        request.setQuantity(4);
-
-        RequestResponse response = new RequestResponse();
-        response.setStatusToSuccess();
-        when(inventoryService.updateInventory(request)).thenThrow(new InvalidRequestException());
-
-        mvc.perform(post("/inventory/edit")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("id", "1")
-                .param("name","name2")
-                .param("category_id", "1")
-                .param("type","Consumable")
-                .param("quantity","4")
-        )
-                .andExpect(status().isBadRequest());
-
-    }
+    //////////////////////////////////////////////////////////////////////////
+    //public RequestResponse removeInventory(@PathVariable("id") Integer id)//
+    //////////////////////////////////////////////////////////////////////////
 
     @Test
-    public void deleteInventory_success() throws Exception {
+    public void deleteInventory_test() throws Exception {
         RequestResponse response = new RequestResponse();
         response.setStatusToSuccess();
 
@@ -212,65 +161,9 @@ public class InventoryControllerTest {
 
     }
 
-    @Test
-    public void deleteInventory_invalidRequest() throws Exception {
-        when(inventoryService.deleteInventory(anyInt())).thenThrow(new InvalidRequestException());
-
-        mvc.perform(post("/inventory/delete/1"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.success").value(false));
-
-        verify(inventoryService,times(1)).deleteInventory(anyInt());
-
-    }
-
-    @Test
-    public void deleteInventory_dataNotFound() throws Exception {
-        when(inventoryService.deleteInventory(anyInt())).thenThrow(new DataNotFoundException());
-
-        mvc.perform(post("/inventory/delete/1"))
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.success").value(false));
-
-        verify(inventoryService,times(1)).deleteInventory(anyInt());
-
-    }
-
-    @Test
-    @WithMockUser(roles = "ADMIN")
-    public void getInventoryDetail_success() throws Exception{
-        Category category = new Category().builder().id(1).name("/all").build();
-        InventoryDTO inventory = InventoryDTO.builder()
-                .id(1)
-                .name("name")
-                .quantity(2)
-                .type(InventoryType.Consumable.toString())
-                .category(category.getName())
-                .category_id(category.getId())
-                .build();
-
-        System.out.println(inventory);
-
-        when(inventoryService.getById(1)).thenReturn(inventory);
-        when(userService.currentUserIsAdmin()).thenReturn(true);
-
-        mvc.perform(get("/inventory/detail/{id}", 1)
-        )
-                .andExpect(status().isOk());
-
-        verify(inventoryService, times(1)).getById(1);
-    }
-
-    @Test
-    public void getInventoryDetail_dataNotFound() throws Exception{
-        when(inventoryService.getById(1)).thenThrow(new DataNotFoundException());
-
-        mvc.perform(get("/inventory/detail/{id}", 1))
-                .andExpect(status().isNotFound());
-
-        verify(inventoryService, times(1)).getById(1);
-        Mockito.verifyZeroInteractions(inventoryService);
-    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //public SearchResponse searchInventory(@RequestParam("search")String query, @RequestParam("page")Integer page, @RequestParam("length")Integer length)//
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Test
     public void searchInventory_test() throws Exception {
@@ -290,6 +183,10 @@ public class InventoryControllerTest {
 
         verify(inventoryService,times(1)).getSearchResult(any());
     }
+
+    ///////////////////////////////////////////////////////////////////////////
+    //public String downloadInventoryDocument(@PathVariable("id") Integer id)//
+    ///////////////////////////////////////////////////////////////////////////
 
     @Test
     public void downloadInventoryDocument_test() throws Exception {
@@ -317,6 +214,10 @@ public class InventoryControllerTest {
 
     }
 
+    //////////////////////////////////////////////////////////////////////////////////////
+    //public RequestResponse uploadInventories(@RequestParam("file") MultipartFile file)//
+    //////////////////////////////////////////////////////////////////////////////////////
+
     @Test
     public void uploadInventories_test() throws Exception {
         String content = "name,price,quantity,type,description\n" +
@@ -334,6 +235,71 @@ public class InventoryControllerTest {
         mvc.perform(MockMvcRequestBuilders.multipart("/inventory/upload")
                 .file(file))
                 .andExpect(status().isOk());
-
     }
+
+    /////////////////////////////////////////////////////////////////////////////////
+    //public String getInventoryDetail(Model model, @PathVariable("id") Integer id)//
+    /////////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void getInventoryDetail_test() throws Exception{
+        Category category = new Category().builder().id(1).name("/all").build();
+        InventoryDTO inventory = InventoryDTO.builder()
+                .id(1)
+                .name("name")
+                .quantity(2)
+                .type(InventoryType.Consumable.toString())
+                .category(category.getName())
+                .category_id(category.getId())
+                .build();
+
+        System.out.println(inventory);
+
+        when(inventoryService.getById(1)).thenReturn(inventory);
+        when(userService.currentUserIsAdmin()).thenReturn(true);
+
+        mvc.perform(get("/inventory/detail/{id}", 1)
+        )
+                .andExpect(status().isOk());
+
+        verify(inventoryService, times(1)).getById(1);
+    }
+
+    ////////////////////////////////////////////////
+    //public String getInventoryTable(Model model)//
+    ////////////////////////////////////////////////
+
+    @Test
+    public void getInventoryTable_test() throws Exception {
+        List<CategoryDTO> categoryDTOS = new ArrayList<>();
+        categoryDTOS.add(CategoryDTO.builder().id(1).name("/all").build());
+        List<InventoryDTO> list = new ArrayList<>();
+        list.add(InventoryDTO.builder()
+                .id(1)
+                .name("name")
+                .quantity(2)
+                .type(InventoryType.Consumable.toString())
+                .category("/all")
+                .category_id(1)
+                .build());
+
+        System.out.println(list);
+
+        when(inventoryService.getAll()).thenReturn(list);
+        when(itemCategoryService.getAllItemCategory(anyBoolean())).thenReturn(categoryDTOS);
+
+        mvc.perform(get("/inventory/all")
+        )
+                .andExpect(status().isOk());
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //public DataTablesResult<InventoryDataTableResponse> getPaginatedInventories(HttpServletRequest servletRequest)//
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    public void testGetPaginatedInventories_test() throws Exception {
+    }
+
 }
