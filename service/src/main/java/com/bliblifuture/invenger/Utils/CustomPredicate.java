e64346baeda8c78d65d7c573510242526d57e29d
Unknown <alfian853@gmail.com>
2019-01-03 12:36:48 +0700

    add @FunctionalInterface

--- service/src/main/java/com/bliblifuture/invenger/Utils/CustomPredicate.java
+++ service/src/main/java/com/bliblifuture/invenger/Utils/CustomPredicate.java
@@ -5,6 +5,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.persistence.criteria.Predicate;
 
+@FunctionalInterface
 public interface CustomPredicate {
     Predicate getPredicate(Root root, CriteriaQuery criteriaQuery, CriteriaBuilder criteriaBuilder);
 }
